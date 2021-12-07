b3d2295075850560adafaddcb27d23268ffc9fd0
Quentin7305 <93937329+Quentin7305@users.noreply.github.com>
2021-12-07 16:41:46 +0000

    Update Opening.py

--- src/Opening.py
+++ src/Opening.py
@@ -47,13 +47,35 @@ class Opening:
     # Defines the vertices and faces        
     def generate(self):
         self.vertices = [ 
-                # Définir ici les sommets
+                [0, 0, 0 ], 
+                [0, 0, self.parameters['height']], 
+                [self.parameters['width'], 0, self.parameters['height']],
+                [self.parameters['width'], 0, 0],      
+                [0, self.parameters['thickness'], 0], 
+                [0, self.parameters['thickness'], self.parameters['height']], 
+                [self.parameters['width'], self.parameters['thickness'], self.parameters['height']],
+                [self.parameters['width'], self.parameters['thickness'], 0]
                 ]
-        self.faces = [
-                # définir ici les faces
-                ]   
+        self.faces = [               
+                [1, 0, 4, 5],               
+                [4, 0, 3, 7],
+                [7, 3, 2, 6],
+                [6, 2, 1, 5]
+                ]    
         
     # Draws the faces                
     def draw(self):        
         # A compléter en remplaçant pass par votre code
-        pass
+        gl.glPushMatrix()
+        gl.glTranslatef(self.parameters['position'][0],
+                        self.parameters['position'][1],
+                        self.parameters['position'][2])
+            
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL) 
+        gl.glBegin(gl.GL_QUADS)
+        gl.glColor3fv(self.parameters['color'])
+        for face in self.faces:
+            for vertex in face:
+                gl.glVertex3fv(self.vertices[vertex])  
+        gl.glEnd()  
+        gl.glPopMatrix()
